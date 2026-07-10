c87440dab3ad6945f36027f550586aaa058380ee
Albert <114008677+albert-hendrych@users.noreply.github.com>
2026-07-10 21:05:12 +0200

    Updated sheets_db.py

--- sheets_db.py
+++ sheets_db.py
@@ -1,10 +1,27 @@
-def obtener_notas(asignatura):
+import os
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+
+def obtener_cliente():
+    """Conecta a Google Sheets. Prioriza la variable de entorno por seguridad."""
+    json_creds = os.getenv("GOOGLE_CREDENTIALS_JSON")
+    if json_creds:
+        creds_dict = json.loads(json_creds)
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, SCOPES)
+    else:
+        creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", SCOPES)
+    return gspread.authorize(creds)
+
+def obtener_notas():
     """
-    Se conectará a Google Sheets usando gspread para leer las notas.
+    Se conecta a Google Sheets usando gspread para leer las notas.
     """
     # Lógica futura: 
-    # gc = gspread.service_account(filename=GOOGLE_CREDENTIALS_FILE)
-    # sh = gc.open_by_url(SPREADSHEET_URL)
+    # gc = obtener_cliente()
+    # sh = gc.open_by_url(os.getenv("SPREADSHEET_URL"))
     # worksheet = sh.sheet1
     # ... buscar la asignatura y devolver los datos
     pass
